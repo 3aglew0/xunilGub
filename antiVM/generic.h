e882c30235f29719d310d072862bb6ae32fda321
Marco Brotto <marco@pc254.home>
2021-05-31 11:10:34 +0200

    add mouse movement with /dev/input logic

--- antiVM/generic.h
+++ antiVM/generic.h
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <array>
 #include "unistd.h"
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/wait.h>
 
 std::string execCommand(const std::string, int&); 
 bool mouse_movement();
+bool mouse_movement_notool();
