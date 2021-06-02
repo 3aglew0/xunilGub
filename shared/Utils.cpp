10135b56d1aa41d08eacf11cb58eead76b013c03
Noemi Daniele <noemi.daniele@eurecom.fr>
2021-06-02 15:18:54 +0200

    added gdt, idt, ldt checks

--- shared/Utils.cpp
+++ shared/Utils.cpp
@@ -1,12 +1,32 @@
 #include "pch.h"
 #include "Utils.h"
+typedef struct
+{
+	// Lots of *really bad* examples on the web where they use char[2]
+	// to store the output of SIDT and SGDT.  This is wrong!
+	//
+	// In 32-bit mode, the output is 6 bytes, where the first 2 is
+	// the limit and the upper 4 bytes is the 32-bit base address.
+	//
+	// In 64-bit mode, the output is 10 bytes, where the first 2 is
+	// the limit and the upper 8 bytes is the 64-bit base address.
+	//
+	// This is described on page 299 of AMD64 Architecture Programmer's
+	// Manual Volume 3: General-Purpose and System Instructions:
+	// http://www.amd.com/us-en/assets/content_type/white_papers_and_tech_docs/24594.pdf
+
+	ulong	limit : 16;
+	ulong	base  : 64;
+} __attribute__((packed)) dt;
 
 ulong get_idt_base()
 {
 	// Get the base of Interupt Descriptor Table (IDT)
 
-	u_char idtr[6];
-	ulong idt = 0;
+	//unsigned char idtr[6];
+	unsigned long idtr;
+	ulong idt_ = 0;
+    dt idt = {0, 0};
 
 	// sidt instruction stores the contents of the IDT Register
 	// (the IDTR which points to the IDT) in a processor register.
@@ -14,10 +34,12 @@ ulong get_idt_base()
 #if defined (ENV32BIT)
 	_asm sidt idtr
 #endif
-	idt = *((unsigned long *)&idtr[2]);
-	// printf("IDT base: 0x%x\n", idt);
+    asm volatile( "sidt %0" : "=m"(idt) );
+    idtr = idt.base;
+	//idt_ = *((unsigned long *)&idtr[2]);
+	printf("IDT base: 0x%x\n", idtr);
 
-	return idt;
+	return idtr;
 }
 
 
@@ -30,9 +52,7 @@ ulong get_ldt_base()
 
 	// sldt instruction stores the contents of the LDT Register
 	// (the LDTR which points to the LDT) in a processor register.
-#if defined (ENV32BIT)
-	_asm sldt ldtr
-#endif
+    asm volatile( "sldt %0" : "=m"(ldtr) );
 	ldt = *((unsigned long *)&ldtr[0]);
 	printf("LDT base: 0x%x\n", ldt);
 
@@ -44,16 +64,22 @@ ulong get_gdt_base()
 {
 	// Get the base of Global Descriptor Table (GDT)
 
-	u_char gdtr[6];
-	ulong gdt = 0;
+	// u_char gdtr[6];
+	// ulong gdt = 0;
 
 	// sgdt instruction stores the contents of the GDT Register
 	// (the GDTR which points to the GDT) in a processor register.
 #if defined (ENV32BIT)
 	_asm sgdt gdtr
 #endif
-	gdt = *((unsigned long *)&gdtr[2]);
-	// printf("GDT base: 0x%x\n", gdt);
+	// asm volatile( "sgdt %0" : "=m"(gdtr) );
+	// gdt = *((unsigned long *)&gdtr[2]);
+	//  printf("GDT base: 0x%x\n", gdt);
 
-	return gdt;
+	dt gdt = {0, 0};
+	asm volatile( "sgdt %0" : "=m"(gdt) );
+	printf( "GDT: limit=%04d, base=%016lx\n", gdt.limit, gdt.base );
+
+	//return gdt;
+	return gdt.base;
 }
